f9846a701080ffd93e37e6b7b7f3b15643d50082
GinoGallardo <ginosgt@gmail.com>
2024-10-09 16:40:34 -0300

    establesiendo rutas

--- public/js/navBar.js
+++ public/js/navBar.js
@@ -2,14 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
   const navbarNav = document.getElementById('navbarNav'); // Asegúrate de que uses el ID correcto 'navbarNav'
 
   let items = [
-    { name: 'Home', href: '/ecommers/views/index.html' },
+    { name: 'Home', href: '/ecommers/index.html' },
     { name: 'PC Armadas', href: '/ecommers/views/pcArmadas.html' },
     { name: 'Monitores', href: '/ecommers/views/monitores.html' },
     { name: 'Periféricos', href: '/ecommers/views/perifericos.html' },
     { name: 'Sillas Gamer', href: '/ecommers/views/sillasGamer.html' },
     { name: 'Contacto', href: '/ecommers/views/contacto.html' }
   ];
-
   function headers(items) { 
     const ul = document.createElement('ul');
     ul.classList.add('navbar-nav'); // Añadir las clases necesarias
